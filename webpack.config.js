824519b066190b4e5ac7749eaae1fb221c326b5a
kengleng <malawueng@gmail.com>
2016-06-26 21:49:02 +0800

    before Font Awesome

--- webpack.config.js
+++ webpack.config.js
@@ -13,13 +13,77 @@ const dest    = join(root, 'dist');
 
 const NODE_ENV = process.env.NODE_ENV;
 const isDev = NODE_ENV === 'development';
-// alternatively, we can use process.argv[1]
-// const isDev = (process.argv[1] || '')
-//                .indexOf('hjs-dev-server') !== -1;
+const dotenv = require('dotenv');
+const dotEnvVars = dotenv.config();
+const environmentEnv = dotenv.config({
+  path: join(root, 'config', `${NODE_ENV}.config.js`),
+  silent: true,
+});
+const envVariables =
+    Object.assign({}, dotEnvVars, environmentEnv);
+
+const defines =
+  Object.keys(envVariables)
+  .reduce((memo, key) => {
+    const val = JSON.stringify(envVariables[key]);
+    memo[`__${key.toUpperCase()}__`] = val;
+    return memo;
+  }, {
+    __NODE_ENV__: JSON.stringify(NODE_ENV)
+  });
+
 var config = getConfig({
   in: join(__dirname, 'src/app.js'),
   out: join(__dirname, 'dist'),
   clearBeforeBuild: true
+});
+
+  config.plugins = [
+    new webpack.DefinePlugin(defines)
+  ].concat(config.plugins);
+
+
+
+// alternatively, we can use process.argv[1]
+// const isDev = (process.argv[1] || '')
+//                .indexOf('hjs-dev-server') !== -1;
+
+config.postcss = [].concat([
+  require('precss')({}),
+  require('autoprefixer')({}),
+  require('cssnano')({})
+]);
+
+const cssModulesNames = `${isDev ? '[path][name]__[local]__' : ''}[hash:base64:5]`;
+
+const matchCssLoaders = /(^|!)(css-loader)($|!)/;
+
+const findLoader = (loaders, match) => {
+  const found = loaders.filter(l => l &&
+      l.loader && l.loader.match(match));
+  return found ? found[0] : null;
+}
+// existing css loader
+const cssloader =
+  findLoader(config.module.loaders, matchCssLoaders);
+
+const newloader = Object.assign({}, cssloader, {
+  test: /\.module\.css$/,
+  include: [src],
+  loader: cssloader.loader
+    .replace(matchCssLoaders,
+    `$1$2?modules&localIdentName=${cssModulesNames}$3`)
 })
+config.module.loaders.push(newloader);
+
+config.module.loaders.push({
+  test: /\.css$/,
+  include: [modules],
+  loader: 'style!css'
+});
+
+cssloader.test =
+  new RegExp(`[^module]${cssloader.test.source}`)
+cssloader.loader = newloader.loader
 
 module.exports = config;
